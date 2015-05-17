1a82e2844a3db2783cb6963cadc48ec3704eb3c7
tzermias <zacharias.tzermias@gmail.com>
2015-05-18 00:38:26 +0300

    More commits.

--- README.md
+++ README.md
@@ -5,4 +5,41 @@ A simple sketch that displays temperature and humidity from a DHT22
 sensor to a TM1637 display.
 
 
-// vi: tw=72
+### Commands for RRDtool ###
+
+1. Create an RRD database.
+
+    rrdtool create test.rrd --step 5 \
+        DS:temp:GAUGE:20:0:U \
+        DS:hum:GAUGE:20:0:100 \
+        DS:hi:GAUGE:20:0:U \
+        RRA:AVERAGE:0.5:1:1000 \
+
+2. Use the following script to insert data from the Arduino to the DB.
+
+    while true
+    do 
+        rrdtool update test.rrd $(curl  -q http://arduino.local/data/get/ 2>/dev/null |./readSensor.py) && \
+        echo "Updated" && sleep 5 
+    done
+
+3. Plot a graph using the following command.
+
+    rrdtool graph test.png --rigid --lower-limit 0 --upper-limit 100 \
+    --vertical-label 'Temperature in C' --right-axis-label 'Percent' \
+    --right-axis 2.5:0 --start -900 --end now \
+    DEF:temp=test.rrd:temp:AVERAGE LINE:temp#54EC48:"Temperature":STACK \
+    GPRINT:temp:MIN:"min\: %3.2lf %s" \
+    GPRINT:temp:AVERAGE:"average\: %3.2lf %s" \
+    GPRINT:temp:MAX:"max\:%3.2lf %s\n" \
+    DEF:hum=test.rrd:hum:AVERAGE LINE:hum#8BBFFF:"Humidity":STACK \
+    GPRINT:hum:MIN:"min\: %3.2lf %s" \
+    GPRINT:hum:AVERAGE:"average\: %3.2lf %s" \
+    GPRINT:hum:MAX:"max\: %3.2lf %s\n" \
+    DEF:hi=test.rrd:hi:AVERAGE LINE:hi#F29D00:"Heat Index":STACK \
+    GPRINT:hi:MIN:"min\: %3.2lf %s" \
+    GPRINT:hi:AVERAGE:"average\: %3.2lf %s" \
+    GPRINT:hi:MAX:"max\: %3.2lf %s\n"
+
+<!-- vi: tw=72
+-->
--- dht22_display.ino
+++ dht22_display.ino
@@ -2,14 +2,14 @@
 #include <TM1637Display.h>
 #include "DHT.h"
 #include <Process.h>
-
+#include <Bridge.h>
 
 //Board connections
 #define DHTPIN 2     
 #define CLK 4
 #define DIO 3
 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTTYPE DHT22
 
 #define DELAY 1000
 
@@ -17,12 +17,13 @@
 DHT dht(DHTPIN, DHTTYPE);
 TM1637Display display(CLK, DIO);
 
-//Temperature variable
-float t,h;
-//Variable for date
+float t,h,f,hi;
 Process date;
 int hours,minutes;
-int lastSecond =-1;
+unsigned long previousMillis = 0;
+unsigned long previousMillis2 = 0;
+int nextSegment = 1;
+String date_string;
 
 void setup() {
   Bridge.begin();
@@ -30,76 +31,82 @@ void setup() {
   Serial.println("DHT22 + DISPLAY");
  
   dht.begin();
-  //Initial measurement of temperature
-  t = dht.readTemperature();
-  h = dht.readHumidity();
   
-  //Get dat from network
-  if (!date.running()) {
-    date.begin("date");
-    date.addParameter("+%H%M");
-    date.run();
-  }
+  display.setBrightness(0x0a);
+  
+  //Check date for the first time
+  date.begin("date");
+  date.addParameter("+%H%M");
+  date.runAsynchronously();
+  date_string = date.readString();
 }
 
 void loop() {
-  display.setBrightness(0x0a);
+  unsigned long currentMillis = millis();
   
-  //Display temperature on TM1637
-
-  display.showNumberDec(t,false);
-
-  delay(DELAY);
-  display.showNumberDec(h,false);
-  //display.showNumberDec(25,false);
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   h = dht.readHumidity();
   // Read temperature as Celsius
   t = dht.readTemperature();
   // Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
+  f = dht.readTemperature(true);
   
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  
-
-
   // Compute heat index
   // Must send in temp in Fahrenheit!
-  float hi = dht.computeHeatIndex(f, h);
-
-  Serial.print("[");
-  Serial.print(date.readString());
-  Serial.print("]");
-  Serial.print("Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: "); 
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hi);
-  Serial.println(" *F");
+  hi = dht.computeHeatIndex(f, h);
   
-  
-  if (!date.running()) {
+  //Check date here every 3 s
+  if (currentMillis - previousMillis2 >= 3*DELAY && !date.running()) {
+    previousMillis2=currentMillis;
     date.begin("date");
     date.addParameter("+%H%M");
-    date.run();
+    date.runAsynchronously();
+    date_string = date.readString();
   }
   
-  String s=date.readString();
-  display.showNumberDec(s.toInt(),true);
-  //Light-up middle dots
-  //http://forum.dev.arduino.cc/index.php?topic=271238.0
-  uint8_t segto;
-  segto=0x80 | display.encodeDigit((s.toInt()/100)%10);
-  display.setSegments(&segto,1,1);
+  if(currentMillis - previousMillis >= DELAY) {
+    previousMillis = currentMillis;
+    
+    //Show temperature, humidity and clock in TM1637
+    if(nextSegment == 1) {
+      display.showNumberDec(t,false);
+      nextSegment = 2;
+    }else if (nextSegment == 2) {
+      display.showNumberDec(h,false);
+      nextSegment = 3;
+    }else if (nextSegment == 3) {
+      //show date
+      display.showNumberDec(date_string.toInt(),true);
+      //Light-up middle dots
+      //http://forum.dev.arduino.cc/index.php?topic=271238.0
+      uint8_t segto;
+      segto=0x80 | display.encodeDigit((date_string.toInt()/100)%10);
+      display.setSegments(&segto,1,1);
+
+      nextSegment = 1;
+    }
+    
+    //Display values in Serial
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t");
+    Serial.print("Heat index: ");
+    Serial.print(hi);
+    Serial.println(" *F");
+    
+    //TEST
+    Bridge.put("temperature", String(t));
+    Bridge.put("humidity", String(h));
+    Bridge.put("heatIndex", String(hi));
+  }
   
 }
